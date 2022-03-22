13380276f3392ab0fc5e73a380df4b19e9f00e0c
MSSkowron <92554920+MSSkowron@users.noreply.github.com>
2022-03-22 23:27:19 +0100

    Add files via upload

---
+++ cw03/zad1/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc,char** argv)
+{
+    if(argc != 2)
+    {
+        printf("There has to be exactly one argument which is number of processes to run!.\n");
+        exit(1);
+    }
+    int length = strlen(argv[1]);
+    for (int i = 0; i < length; i++)
+        if (!isdigit(argv[1][i]))
+        {
+            printf ("Entered input is not a number\n");
+            exit(1);
+        }
+
+    pid_t child_pid;
+    printf("\nPID glownego programu: %d\n", (int)getpid());
+
+    int range = atoi(argv[1]);
+
+    for (int i = 0; i < range; i++)
+    {
+        child_pid = fork();
+        if(child_pid == 0 ) {
+            execvp("./child", NULL);
+        }
+        wait(NULL);
+    }
+
+    return 0;
+}
---
+++ cw03/zad3/main.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+
+#define LENGTH 50
+
+int main(int argc,char** argv)
+{
+    if(argc != 4)
+    {
+        printf("There has to be exactly three argument: directory, string, depth!.\n");
+        exit(1);
+    }
+    int length = strlen(argv[3]);
+    for (int i = 0; i < length; i++)
+    {
+        if (!isdigit(argv[3][i])) {
+            printf("Entered depth is not a number\n");
+            exit(1);
+        }
+    }
+
+    struct dirent * dp;
+    DIR* dfd;
+    char* dir;
+    char* str;
+    int max_depth;
+    pid_t child_pid;
+    char filename_qfd[257];
+
+    dir = argv[1];
+    str = argv[2];
+    max_depth = atoi(argv[3]);
+
+    if((dfd= opendir(dir)) == NULL)
+    {
+        fprintf(stderr,"Can't open %s\n",dir);
+        exit(1);
+    }
+
+    while ((dp = readdir(dfd)) != NULL)
+    {
+        if(strcmp("..",dp->d_name)==0 || strcmp(".",dp->d_name)==0)
+        {
+            continue;
+        }
+
+        struct stat stbuf;
+        sprintf( filename_qfd , "%s/%s",dir,dp->d_name);
+
+        if( lstat(filename_qfd,&stbuf ) < 0 )
+        {
+            printf("\n------\n");
+            printf("\nUnable to stat file: %s",filename_qfd) ;
+            continue ;
+        }
+
+        if(S_ISREG(stbuf.st_mode))
+        {
+            FILE* filePointer;
+            int bufferLength = 256;
+            char line[bufferLength];
+
+            filePointer = fopen(filename_qfd, "r");
+            while(fgets(line, bufferLength, filePointer))
+            {
+                char *ptr = strstr(line, str);
+                if (ptr != NULL)
+                {
+                    printf("\nZNALEZIONO!");
+                    printf("\nSciezka: %s",filename_qfd);
+                    printf("\nPID: %d\n",(int)getpid());
+                    break;
+                }
+            }
+            fclose(filePointer);
+        }
+    }
+
+    char* directory = (char *) calloc(257,sizeof(char));
+    char* word = (char *) calloc(LENGTH,sizeof(char));
+    char* depth = (char *) calloc(LENGTH,sizeof(char));
+    char* maximum_depth = (char *) calloc(LENGTH,sizeof(char));
+
+    rewinddir(dfd);
+    while ((dp = readdir(dfd)) != NULL)
+    {
+        if(strcmp("..",dp->d_name)==0 || strcmp(".",dp->d_name)==0)
+        {
+            continue;
+        }
+        struct stat stbuf;
+        sprintf( filename_qfd , "%s/%s",dir,dp->d_name);
+        if( lstat(filename_qfd,&stbuf ) < 0 )
+        {
+            printf("\n------\n");
+            printf("\nUnable to stat file: %s",filename_qfd) ;
+            continue ;
+        }
+        if(S_ISDIR(stbuf.st_mode))
+        {
+            snprintf(directory, 257, "%s", filename_qfd);
+            snprintf(word, LENGTH, "%s", str);
+            snprintf(depth, LENGTH, "%d", 2);
+            snprintf(maximum_depth, LENGTH, "%d", max_depth);
+
+            char *const parmList[] = {directory, word, depth,maximum_depth,NULL};
+
+            child_pid = fork();
+            if(child_pid == 0 ) {
+                execv("./child", parmList);
+            }
+            wait(NULL);
+        }
+        memset(directory, 0, LENGTH);
+        memset(word, 0, LENGTH);
+        memset(depth, 0, LENGTH);
+    }
+
+    free(directory);
+    free(word);
+    free(depth);
+    free(maximum_depth);
+
+    closedir(dfd);
+    return 0;
+}
